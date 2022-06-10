bbddf14b346ebbe85af08b132ae1c02408566b4d
Foreinyel <Foreinyel@163.com>
2022-06-11 02:18:50 +0800

    props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Component from "./Component.svelte";
 
 const mount = (el, props) => {
   console.log(props)
+  console.log(props.children.type(props.children.props))
   new Component({
     target: el,
     props: {
